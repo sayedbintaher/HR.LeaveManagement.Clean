ec9b97251c2111c27b5fb55967aadb45180fb7e4
sayedbintaher <sayedbintaher@gmail.com>
2024-01-22 00:57:42 +0600

    Query and Query Hander added

--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypeDetails/LeaveTypeDetailsDto.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypeDetails/LeaveTypeDetailsDto.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HR.LeaveManagement.Domain.Common
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypeDetails
 {
-    public abstract class BaseEntity
+    public class LeaveTypeDetailsDto
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
         public DateTime? DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
     }
